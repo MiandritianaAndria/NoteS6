9c6e5ad08f502b171bb022ebbbacd2b6a523593a
kathyrazaf01 <120714714+kathyrazaf01@users.noreply.github.com>
2024-07-10 19:07:29 +0300

    Update ProjetNainaApplication.java

--- src/main/java/itu/examen/projetnaina/ProjetNainaApplication.java
+++ src/main/java/itu/examen/projetnaina/ProjetNainaApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EntityScan(basePackages = "itu.examen.projetnaina.model")
-@EnableJpaRepositories(basePackages = "itu.examen.projetnaina.repository")
-@ComponentScan(basePackages = "itu.examen.projetnaina")
 public class ProjetNainaApplication {
 
     public static void main(String[] args) {
